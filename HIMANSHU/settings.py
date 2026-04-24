8cae0b318c2542a6cb2356e88a1776dcd9868811
Himanshu8578 <himanshukurre9999@gmail.com>
2026-04-24 21:26:12 +0530

    added requirements

--- HIMANSHU/settings.py
+++ HIMANSHU/settings.py
@@ -132,3 +132,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
